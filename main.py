975b05986654b2e954e7c1221567c85e55efd5f0
Nabilath <nabilath.nachirou@gmail.com>
2026-01-16 16:40:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.utils import print_hello_world
+from PIL import Image
+
+def main():
+    print_hello_world()
+    
+    image = Image.open("dataset/image.jpg")
+    print(f"Image size: {image.size}")
+    
+    
+if __name__ == "__main__":
+    main()
