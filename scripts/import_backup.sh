93adf7b66316e0ac15d8d141f2a0f9b18cbc7ab7
nath54 <cerisara.nathan@gmail.com>
2026-02-10 11:28:59 +0100

    Nathan: add buildkit

--- scripts/import_backup.sh
+++ scripts/import_backup.sh
@@ -33,7 +33,7 @@ echo "🔄 Importing MLflow data from: $BACKUP_FILE"
 echo ""
 
 # Run import with remaining arguments
-docker-compose run --rm \
+DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose run --rm \
     -e BACKUP_FILE="$BACKUP_FILE" \
     import python scripts/import_mlflow.py /app/backups/"$BACKUP_FILE" "$@"
 
